c7106da153d8a82539805f0bf7448d2870a7acc4
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-02-05 12:14:25 +0100

    more vocabularies

--- esdrt/content/vocabularies.py
+++ esdrt/content/vocabularies.py
@@ -19,3 +19,75 @@ class MSVocabulary(object):
         return SimpleVocabulary(terms)
 
 grok.global_utility(MSVocabulary, name=u"esdrt.content.eu_member_states")
+
+
+class GHGSourceCategory(object):
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        pvoc = getToolByName(context, 'portal_vocabularies')
+        voc = pvoc.getVocabularyByName('ghg_source_category')
+        terms = []
+        if voc is not None:
+            for key, value in voc.getVocabularyLines():
+                # create a term - the arguments are the value, the token, and
+                # the title (optional)
+                terms.append(SimpleVocabulary.createTerm(key, key, value))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(GHGSourceCategory,
+    name=u"esdrt.content.ghg_source_category")
+
+
+class GHGSourceSectors(object):
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        pvoc = getToolByName(context, 'portal_vocabularies')
+        voc = pvoc.getVocabularyByName('ghg_source_sectors')
+        terms = []
+        if voc is not None:
+            for key, value in voc.getVocabularyLines():
+                # create a term - the arguments are the value, the token, and
+                # the title (optional)
+                terms.append(SimpleVocabulary.createTerm(key, key, value))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(GHGSourceSectors,
+    name=u"esdrt.content.ghg_source_sectors")
+
+
+class StatusFlag(object):
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        pvoc = getToolByName(context, 'portal_vocabularies')
+        voc = pvoc.getVocabularyByName('status_flag')
+        terms = []
+        if voc is not None:
+            for key, value in voc.getVocabularyLines():
+                # create a term - the arguments are the value, the token, and
+                # the title (optional)
+                terms.append(SimpleVocabulary.createTerm(key, key, value))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(StatusFlag,
+    name=u"esdrt.content.status_flag")
+
+
+class CRFCode(object):
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        pvoc = getToolByName(context, 'portal_vocabularies')
+        voc = pvoc.getVocabularyByName('crf_code')
+        terms = []
+        if voc is not None:
+            for key, value in voc.getVocabularyLines():
+                # create a term - the arguments are the value, the token, and
+                # the title (optional)
+                terms.append(SimpleVocabulary.createTerm(key, key, value))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(CRFCode,
+    name=u"esdrt.content.crf_code")
