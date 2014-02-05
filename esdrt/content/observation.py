c7ba49aa6e293acb8d0d33bd0e2c217f117a6e5e
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-02-05 16:55:32 +0100

    add year field

--- esdrt/content/observation.py
+++ esdrt/content/observation.py
@@ -1,27 +1,13 @@
+from datetime import datetime
+from esdrt.content import MessageFactory as _
+from five import grok
+from plone.app.textfield import RichText
+from plone.directives import dexterity, form
+from plone.directives.form import default_value
+from plone.namedfile.interfaces import IImageScaleTraversable
+from zope import schema
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
-from Products.CMFCore.utils import getToolByName
-from five import grok
-from plone.directives import dexterity, form
-
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
-from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from esdrt.content import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -33,6 +19,10 @@ class IObservation(form.Schema, IImageScaleTraversable):
         title=_(u"Country"),
         vocabulary='esdrt.content.eu_member_states',
 
+    )
+
+    year = schema.Int(
+        title=_(u'Observation year'),
     )
 
     crf_code = schema.Choice(
@@ -58,6 +48,11 @@ class IObservation(form.Schema, IImageScaleTraversable):
         vocabulary='esdrt.content.status_flag',
 
     )
+
+
+@default_value(field=IObservation['year'])
+def year_default_value(data):
+    return datetime.now().year - 1
 
 
 class Observation(dexterity.Container):
