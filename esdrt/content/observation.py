92529d295cba96c1bcbed0cfee004a5e382bdeee
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-02-20 16:16:40 +0100

    observation view with change tracking support for description field

--- esdrt/content/observation.py
+++ esdrt/content/observation.py
@@ -1,3 +1,5 @@
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
 from datetime import datetime
 from esdrt.content import MessageFactory as _
 from five import grok
@@ -8,6 +10,9 @@ from plone.namedfile.interfaces import IImageScaleTraversable
 from zope import schema
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
+from zope.i18n import translate
+
+from Products.CMFEditions import CMFEditionsMessageFactory as _CMFE
 
 
 # Interface class; used to define content-type schema.
@@ -106,3 +111,78 @@ class ObservationView(grok.View):
     grok.context(IObservation)
     grok.require('zope2.View')
     grok.name('view')
+
+    def wf_info(self):
+        context = aq_inner(self.context)
+        wf = getToolByName(context, 'portal_workflow')
+        comments = wf.getInfoFor(self.context,
+            'comments', wf_id='esd-review-workflow')
+        actor = wf.getInfoFor(self.context,
+            'actor', wf_id='esd-review-workflow')
+        time = wf.getInfoFor(self.context,
+            'time', wf_id='esd-review-workflow')
+        return {'comments': comments, 'actor': actor, 'time': time}
+
+    @property
+    def repo_tool(self):
+        return getToolByName(self.context, "portal_repository")
+
+
+    def getVersion(self, version):
+        context = aq_inner(self.context)
+        if version == "current":
+            return context
+        else:
+            return self.repo_tool.retrieve(context, int(version)).object
+
+    def versionName(self, version):
+        """
+        Translate the version name. This is needed to allow translation when `version` is the
+        string 'current'.
+        """
+        return _CMFE(version)
+
+    def versionTitle(self, version):
+        version_name = self.versionName(version)
+
+        return translate(
+            _CMFE(u"version ${version}",
+              mapping=dict(version=version_name)),
+            context=self.request
+        )
+
+    def update(self):
+
+        history_metadata = self.repo_tool.getHistoryMetadata(self.context)
+        retrieve = history_metadata.retrieve
+        getId = history_metadata.getVersionId
+        history = self.history = []
+        # Count backwards from most recent to least recent
+        for i in xrange(history_metadata.getLength(countPurged=False)-1, -1, -1):
+            version = retrieve(i, countPurged=False)['metadata'].copy()
+            version['version_id'] = getId(i, countPurged=False)
+            history.append(version)
+        dt = getToolByName(self.context, "portal_diff")
+
+        version1 = self.request.get("one", None)
+        version2 = self.request.get("two", None)
+
+        if version1 is None and version2 is None:
+            self.history.sort(lambda x,y: cmp(x.get('version_id', ''), y.get('version_id')), reverse=True)
+            version1 = self.history[-1].get('version_id', 'current')
+            version2 = self.history[-2].get('version_id', 'current')
+        elif version1 is None:
+            version1 = 'current'
+        elif version2 is None:
+            version2 = 'current'
+
+        self.request.set('one', version1)
+        self.request.set('two', version2)
+
+        changeset = dt.createChangeSet(
+                self.getVersion(version2),
+                self.getVersion(version1),
+                id1=self.versionTitle(version2),
+                id2=self.versionTitle(version1))
+        self.changes = [change for change in changeset.getDiffs()
+                      if not change.same]
