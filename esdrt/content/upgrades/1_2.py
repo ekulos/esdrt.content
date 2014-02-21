dcb7bc384ab74de937f594a740e3a974cdce9036
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-02-21 09:09:58 +0100

    upgrade step to enable spellchecer on install

--- esdrt/content/upgrades/1_2.py
+++ esdrt/content/upgrades/1_2.py
@@ -1,5 +1,6 @@
 from Products.CMFCore.utils import getToolByName
 
+
 PROFILE_ID = 'profile-esdrt.content:default'
 
 
@@ -9,10 +10,19 @@ def upgrade(context, logger=None):
         logger = getLogger('esdrt.content.upgrades.1_2')
 
     upgrade_diff_tool(context, logger)
-    logger.info('Upgraded Diff Tool')
+    enable_atd_spellchecker(context, logger)
+    logger.info('Upgrade step executed')
 
 
 def upgrade_diff_tool(context, logger):
     # Re-run profile installation
     setup = getToolByName(context, 'portal_setup')
     setup.runImportStepFromProfile(PROFILE_ID, 'difftool')
+    logger.info('Upgraded Diff Tool')
+
+
+def enable_atd_spellchecker(context, logger):
+    tinymce = getToolByName(context, 'portal_tinymce')
+    tinymce.libraries_spellchecker_choice = 'AtD'
+    tinymce.libraries_atd_service_url = 'service.afterthedeadline.com'
+    logger.info('Enable AtD spellcheking plugin')
