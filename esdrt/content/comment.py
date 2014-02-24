2479a3a75ce290d2905c9f3327b452c79d02be75
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-02-24 09:30:26 +0100

    remove model and add text field

--- esdrt/content/comment.py
+++ esdrt/content/comment.py
@@ -1,24 +1,10 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
 from five import grok
 from plone.app.textfield import RichText
 from plone.directives import dexterity
 from plone.directives import form
-from plone.formwidget.contenttree import ObjPathSourceBinder
-from plone.namedfile.field import NamedBlobFile
-from plone.namedfile.field import NamedBlobImage
-from plone.namedfile.field import NamedFile
-from plone.namedfile.field import NamedImage
 from plone.namedfile.interfaces import IImageScaleTraversable
-from z3c.form import field
-from z3c.form import group
-from z3c.relationfield.schema import RelationChoice
-from z3c.relationfield.schema import RelationList
-from zope import schema
-from zope.interface import Invalid
-from zope.interface import invariant
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
-from esdrt.content import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -31,8 +17,10 @@ class IComment(form.Schema, IImageScaleTraversable):
     # If you want a model-based interface, edit
     # models/comment.xml to define the content type
     # and add directives here as necessary.
-    form.model("models/comment.xml")
-
+    text = RichText(
+        title=_(u'Text'),
+        required=True,
+        )
 
 # Custom content-type class; objects created for this content type will
 # be instances of this class. Use this class to add content-type specific
@@ -58,3 +46,10 @@ class CommentView(grok.View):
     grok.context(IComment)
     grok.require('zope2.View')
     grok.name('view')
+
+    def render(self):
+        context = aq_inner(self.context)
+        parent = aq_parent(context)
+        url = '%s#%s' % (parent.absolute_url(), context.getId())
+
+        return response.redirect(url)
