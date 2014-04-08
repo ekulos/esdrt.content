e4f07073de51266c42d6e564f44a132d03a5bdc8
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-04-08 10:00:54 +0200

    upgrade step

---
+++ esdrt/content/upgrades/5_6.py
@@ -0,0 +1,45 @@
+from Products.CMFCore.utils import getToolByName
+from esdrt.content.setuphandlers import prepareVocabularies
+
+PROFILE_ID = 'profile-esdrt.content:default'
+
+
+def upgrade(context, logger=None):
+    if logger is None:
+        from logging import getLogger
+        logger = getLogger('esdrt.content.upgrades.5_6')
+
+    reimport_vocabularies(context, logger)
+    upgradeObservations(context, logger)
+    logger.info('Upgrade steps executed')
+
+
+def reimport_vocabularies(context, logger):
+    atvm = getToolByName(context, 'portal_vocabularies')
+    del atvm['eu_member_states']
+    del atvm['fuel']
+    del atvm['gas']
+    prepareVocabularies(context, logger)
+
+
+def upgradeObservations(context, logger):
+    """ check if any observation has old country assignments
+    """
+    COUNTRIES = {
+        'au': 'at',
+        'cr': 'hr',
+        'et': 'ee',
+        'sv': 'sk',
+    }
+
+    brains = context.portal_catalog.unrestrictedSearchResults(
+        portal_type='Observation',
+        Country=COUNTRIES.keys(),
+    )
+    for brain in brains:
+        observation = brain.getObject()
+        old = observation.country
+        new = COUNTRIES.get(old)
+        observation.country = new
+        logger.info('Country set for %s (%s -> %s)' % (observation.id, old, new))
+
