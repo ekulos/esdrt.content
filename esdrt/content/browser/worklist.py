44e8119287c4e458f19ae640edee286335063af9
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-04-10 16:32:36 +0200

    permission check on worklist view

--- esdrt/content/browser/worklist.py
+++ esdrt/content/browser/worklist.py
@@ -1,7 +1,9 @@
 from AccessControl import getSecurityManager
 from five import grok
 from plone import api
+from plone.memoize.view import memoize
 from zope.interface import Interface
+
 
 grok.templatedir('templates')
 
@@ -11,14 +13,31 @@ class WorklistView(grok.View):
     grok.name('worklistview')
     grok.require('zope2.View')
 
+    @memoize
     def get_questions(self):
         catalog = api.portal.get_tool('portal_catalog')
-        values = catalog.searchResults(
-            portal_type='Question',
+        values = catalog.unrestrictedSearchResults(
+            portal_type=['Observation', 'Question'],
             sort_on='modified',
             sort_order='reverse',
         )
-        return [item.getObject() for item in values]
+        items = []
+        user = api.user.get_current()
+        mtool = api.portal.get_tool('portal_membership')
+        for item in values:
+            if 'Manager' in user.getRoles():
+                items.append(item.getObject())
+            else:
+                with api.env.adopt_roles(['Manager']):
+                    try:
+                        obj = item.getObject()
+                        with api.env.adopt_user(user=user):
+                            if mtool.checkPermission('View', obj):
+                                items.append(obj)
+                    except:
+                        pass
+
+        return items
 
     def can_add_observation(self):
         sm = getSecurityManager()
