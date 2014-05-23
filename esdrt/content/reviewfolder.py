6223b51c67a34622194582600e0f4f6a2f52c5c8
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-05-23 13:02:38 +0200

    new content-type to save all observations

--- esdrt/content/reviewfolder.py
+++ esdrt/content/reviewfolder.py
@@ -1,3 +1,6 @@
+from plone.memoize.view import memoize
+from plone import api
+from AccessControl import getSecurityManager
 from five import grok
 from plone.directives import dexterity
 from plone.directives import form
@@ -35,3 +38,43 @@ class ReviewFolderView(grok.View):
     grok.context(IReviewFolder)
     grok.require('zope2.View')
     grok.name('view')
+
+    @memoize
+    def get_questions(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        path = '/'.join(self.context.getPhysicalPath())
+        values = catalog.unrestrictedSearchResults(
+            path=path,
+            portal_type=['Observation', 'Question'],
+            sort_on='modified',
+            sort_order='reverse',
+        )
+        items = []
+        user = api.user.get_current()
+        mtool = api.portal.get_tool('portal_membership')
+        for item in values:
+            if 'Manager' in user.getRoles():
+                items.append(item.getObject())
+            else:
+                with api.env.adopt_roles(['Manager']):
+                    try:
+                        obj = item.getObject()
+                        with api.env.adopt_user(user=user):
+                            if mtool.checkPermission('View', obj):
+                                items.append(obj)
+                    except:
+                        pass
+
+        return items
+
+    def can_add_observation(self):
+        sm = getSecurityManager()
+        return sm.checkPermission('esdrt.content: Add Observation', self)
+
+    def is_secretariat(self):
+        user = api.user.get_current()
+        return 'Manager' in user.getRoles()
+
+    def get_author_name(self, userid):
+        user = api.user.get(userid)
+        return user.getProperty('fullname', userid)
