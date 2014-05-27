2c91cacc0998c3d5c12aa8d3acbea83753d1c6e6
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-05-27 17:08:19 +0200

    reimport vocabularies

--- esdrt/content/upgrades/8_9.py
+++ esdrt/content/upgrades/8_9.py
@@ -1,4 +1,6 @@
 from Products.CMFCore.utils import getToolByName
+from esdrt.content.setuphandlers import prepareVocabularies
+
 
 PROFILE_ID = 'profile-esdrt.content:default'
 
@@ -10,6 +12,22 @@ def upgrade(context, logger=None):
 
     install_workflow(context, logger)
     logger.info('Upgrade steps executed')
+
+
+def reimport_vocabularies(context, logger):
+    atvm = getToolByName(context, 'portal_vocabularies')
+    del atvm['crf_code']
+    del atvm['eea_member_states']
+    del atvm['fuel']
+    del atvm['gas']
+    del atvm['ghg_source_category']
+    del atvm['ghg_source_sectors']
+    del atvm['highlight']
+    del atvm['parameter']
+    del atvm['status_flag']
+    psetup = getToolByName(context, 'portal_setup')
+    profile = psetup._getImportContext(PROFILE_ID)
+    prepareVocabularies(context, profile)
 
 
 def install_workflow(context, logger):
@@ -27,5 +45,6 @@ def install_workflow(context, logger):
     setup.runImportStepFromProfile(PROFILE_ID, 'sharing')
     setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
     logger.info('Reinstalled  Workflows')
+    reimport_vocabularies(context, logger)
     wtool.updateRoleMappings()
     logger.info('Security settings updated')
