ede517560880333c242e84642853ef76f8410db8
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-06-25 16:45:44 +0200

    upgrade steps

---
+++ esdrt/content/upgrades/9_10.py
@@ -0,0 +1,52 @@
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from Products.CMFCore.utils import getToolByName
+
+
+PROFILE_ID = 'profile-esdrt.content:default'
+
+
+def upgrade(context, logger=None):
+    if logger is None:
+        from logging import getLogger
+        logger = getLogger('esdrt.content.upgrades.9_10')
+
+    install_workflow(context, logger)
+    content_types(context, logger)
+    discussion_settings(context, logger)
+    logger.info('Upgrade steps executed')
+
+
+def install_workflow(context, logger):
+    setup = getToolByName(context, 'portal_setup')
+    wtool = getToolByName(context, 'portal_workflow')
+    wtool.manage_delObjects([
+        'esd-question-review-workflow',
+        'esd-review-workflow',
+        'esd-answer-workflow',
+        'esd-comment-workflow',
+        'esd-file-workflow',
+        ])
+    setup.runImportStepFromProfile(PROFILE_ID, 'rolemap')
+    setup.runImportStepFromProfile(PROFILE_ID, 'workflow')
+    setup.runImportStepFromProfile(PROFILE_ID, 'sharing')
+    setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
+    logger.info('Reinstalled  Workflows')
+    wtool.updateRoleMappings()
+    logger.info('Security settings updated')
+
+
+def content_types(context, logger):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
+    setup.runImportStepFromProfile(PROFILE_ID, 'rolemap')
+    setup.runImportStepFromProfile(PROFILE_ID, 'difftool')
+    logger.info('Content-types reinstalled')
+
+
+def discussion_settings(context, logger):
+    registry = getUtility(IRegistry)
+    prefix = 'plone.app.discussion.interfaces.IDiscussionSettings.'
+    registry[prefix + 'globally_enabled'] = True
+    registry[prefix + 'anonymous_comments'] = False
+    registry[prefix + 'moderation_enabled'] = False
