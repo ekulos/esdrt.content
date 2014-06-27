4a07b9fd8c84cc72985425a194b1d23c3ec16420
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-06-27 12:24:05 +0200

    set versioning on upgrade

--- esdrt/content/upgrades/9_10.py
+++ esdrt/content/upgrades/9_10.py
@@ -1,7 +1,7 @@
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
-
+from Products.CMFPlone import PloneMessageFactory as _PMF
 
 PROFILE_ID = 'profile-esdrt.content:default'
 
@@ -14,6 +14,7 @@ def upgrade(context, logger=None):
     install_workflow(context, logger)
     content_types(context, logger)
     discussion_settings(context, logger)
+    set_versioning(context, logger)
     logger.info('Upgrade steps executed')
 
 
@@ -50,3 +51,45 @@ def discussion_settings(context, logger):
     registry[prefix + 'globally_enabled'] = True
     registry[prefix + 'anonymous_comments'] = False
     registry[prefix + 'moderation_enabled'] = False
+
+
+VERSION_POLICIES = [
+        dict(id="off",
+             policy=(),
+             title=_PMF(u"versioning_off",
+                     default=u"No versioning")),
+
+        dict(id="manual",
+             policy=("version_on_revert",),
+             title=_PMF(u"versioning_manual",
+                     default=u"Manual")),
+
+        dict(id="automatic",
+             policy=("at_edit_autoversion", "version_on_revert"),
+             title=_PMF(u"versioning_automatic",
+                     default=u"Automatic")),
+        ]
+
+
+def set_versioning(context, logger):
+    version_policy = 'automatic'
+    portal_repository = getToolByName(context, 'portal_repository')
+    newpolicy = [p for p in VERSION_POLICIES if p["id"] == version_policy][0]
+    versionable_types = list(portal_repository.getVersionableContentTypes())
+    for type_id in ['Comment', 'CommentAnswer']:
+        if not newpolicy["policy"]:
+            if type_id in versionable_types:
+                versionable_types.remove(type_id)
+        else:
+            if type_id not in versionable_types:
+                versionable_types.append(type_id)
+
+        for policy in portal_repository.listPolicies():
+            policy_id = policy.getId()
+            if policy_id in newpolicy["policy"]:
+                portal_repository.addPolicyForContentType(type_id, policy_id)
+            else:
+                portal_repository.removePolicyFromContentType(type_id,
+                    policy_id)
+
+    portal_repository.setVersionableContentTypes(versionable_types)
