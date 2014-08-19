d16ea526c9da165ec8f7e7ff61d0c99ca42edd10
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-08-19 08:41:27 +0200

    reimport also highlight vocabulary. Refs #5e82d3c055c7f52244afd12b59125b7e2671a76e

--- esdrt/content/upgrades/13_14.py
+++ esdrt/content/upgrades/13_14.py
@@ -1,4 +1,5 @@
 from Products.CMFCore.utils import getToolByName
+from esdrt.content.setuphandlers import prepareVocabularies
 
 PROFILE_ID = 'profile-esdrt.content:default'
 
@@ -10,7 +11,16 @@ def upgrade(context, logger=None):
 
     #install_workflow(context, logger)
     css_and_js(context, logger)
+    reimport_vocabularies(context, logger)
     logger.info('Upgrade steps executed')
+
+
+def reimport_vocabularies(context, logger):
+    atvm = getToolByName(context, 'portal_vocabularies')
+    del atvm['highlight']
+    psetup = getToolByName(context, 'portal_setup')
+    profile = psetup._getImportContext(PROFILE_ID)
+    prepareVocabularies(context, profile)
 
 
 def css_and_js(context, logger):
