1df48ef4dcd57693049882b709b51aa67d583e58
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-09-12 09:13:12 +0200

    change subscribers to have 2nd phase states and transitions into account

--- esdrt/content/subscribers.py
+++ esdrt/content/subscribers.py
@@ -10,7 +10,7 @@ from Products.CMFCore.utils import getToolByName
 
 @grok.subscribe(IQuestion, IActionSucceededEvent)
 def question_transition(question, event):
-    if event.action == 'phase1-approve-question':
+    if event.action in ['phase1-approve-question', 'phase2-approve-question']:
         wf = getToolByName(question, 'portal_workflow')
         comment_id = wf.getInfoFor(question,
             'comments', wf_id='esd-question-review-workflow')
@@ -20,7 +20,7 @@ def question_transition(question, event):
             api.content.transition(obj=comment, transition='publish')
             return
 
-    if event.action == 'phase1-recall-question-lr':
+    if event.action in ['phase1-recall-question-lr', 'phase2-recall-question-lr']:
         wf = getToolByName(question, 'portal_workflow')
         comment_id = wf.getInfoFor(question,
             'comments', wf_id='esd-question-review-workflow')
@@ -29,7 +29,7 @@ def question_transition(question, event):
             api.content.transition(obj=comment, transition='retract')
             return
 
-    if event.action == 'phase1-answer-to-lr':
+    if event.action in ['phase1-answer-to-lr', 'phase2-answer-to-lr']:
         wf = getToolByName(question, 'portal_workflow')
         comment_id = wf.getInfoFor(question,
             'comments', wf_id='esd-question-review-workflow')
@@ -39,7 +39,7 @@ def question_transition(question, event):
             api.content.transition(obj=comment, transition='publish')
             return
 
-    if event.action == 'phase1-recall-msa':
+    if event.action in ['phase1-recall-msa', 'phase2-recall-msa']:
         wf = getToolByName(question, 'portal_workflow')
         comment_id = wf.getInfoFor(question,
             'comments', wf_id='esd-question-review-workflow')
@@ -54,6 +54,13 @@ def question_transition(question, event):
             if api.content.get_state(parent) != 'phase1-conclusions':
                 api.content.transition(obj=parent, transition='phase1-draft-conclusions')
 
+    if api.content.get_state(obj=event.object) == 'phase2-closed':
+        parent = aq_parent(event.object)
+        with api.env.adopt_roles(roles=['Manager']):
+            if api.content.get_state(parent) != 'phase2-conclusions':
+                api.content.transition(obj=parent, transition='phase2-draft-conclusions')
+
+
 
 @grok.subscribe(IObservation, IActionSucceededEvent)
 def observation_transition(observation, event):
@@ -64,7 +71,14 @@ def observation_transition(observation, event):
                 q = qs[0]
                 api.content.transition(obj=q, transition='phase1-reopen')
 
-    elif event.action == 'phase1-request-comments':
+    elif event.action == 'phase2-reopen':
+        with api.env.adopt_roles(roles=['Manager']):
+            qs = [q for q in observation.values() if q.portal_type == 'Question']
+            if qs:
+                q = qs[0]
+                api.content.transition(obj=q, transition='phase2-reopen')
+
+    elif event.action in ['phase1-request-comments', 'phase2-request-comments']:
         with api.env.adopt_roles(roles=['Manager']):
             conclusions = [c for c in observation.values() if c.portal_type == 'Conclusion']
             if conclusions:
@@ -72,7 +86,8 @@ def observation_transition(observation, event):
                 api.content.transition(obj=conclusion,
                     transition='request-comments')
 
-    elif event.action == 'phase1-finish-comments':
+
+    elif event.action in ['phase1-finish-comments', 'phase2-finish-comments']:
         with api.env.adopt_roles(roles=['Manager']):
             conclusions = [c for c in observation.values() if c.portal_type == 'Conclusion']
             if conclusions:
@@ -80,7 +95,7 @@ def observation_transition(observation, event):
                 api.content.transition(obj=conclusion,
                     transition='redraft')
 
-    elif event.action == 'phase1-request-close':
+    elif event.action in ['phase1-request-close', 'phase2-request-close']:
         with api.env.adopt_roles(roles=['Manager']):
             conclusions = [c for c in observation.values() if c.portal_type == 'Conclusion']
             if conclusions:
@@ -88,7 +103,7 @@ def observation_transition(observation, event):
                 api.content.transition(obj=conclusion,
                     transition='publish')
 
-    elif event.action == 'phase1-deny-closure':
+    elif event.action in ['phase1-deny-closure', 'phase2-deny-closure']:
         with api.env.adopt_roles(roles=['Manager']):
             conclusions = [c for c in observation.values() if c.portal_type == 'Conclusion']
             if conclusions:
@@ -104,3 +119,12 @@ def observation_transition(observation, event):
                 if api.content.get_state(question) != 'phase1-closed':
                     api.content.transition(obj=question,
                         transition='phase1-close')
+
+    elif event.action == 'phase2-draft-conclusions':
+        with api.env.adopt_roles(roles=['Manager']):
+            questions = [c for c in observation.values() if c.portal_type == 'Question']
+            if questions:
+                question = questions[0]
+                if api.content.get_state(question) != 'phase2-closed':
+                    api.content.transition(obj=question,
+                        transition='phase2-close')
