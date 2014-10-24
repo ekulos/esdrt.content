d9de226ea81662b2a9bd3b897477254efead5454
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-10-24 13:47:50 +0200

    new indexes

--- esdrt/content/indexers.py
+++ esdrt/content/indexers.py
@@ -1,3 +1,4 @@
+from plone.app.discussion.interfaces import IConversation
 from plone.indexer import indexer
 from .observation import IObservation
 
@@ -25,3 +26,55 @@ def observation_ghg_source_sectors(context):
 @indexer(IObservation)
 def observation_status_flag(context):
     return context.status_flag
+
+
+@indexer(IObservation)
+def last_question_reply_number(context):
+    questions = context.values(['Question'])
+    replynum = 0
+    if questions:
+        comments = questions[0].values(['Comment'])
+        if comments:
+            last = comments[-1]
+            disc = IConversation(last)
+            return disc.total_comments
+
+    return replynum
+
+
+@indexer(IObservation)
+def last_answer_reply_number(context):
+    questions = context.values(['Question'])
+    replynum = 0
+    if questions:
+        comments = questions[0].values(['CommentAnswer'])
+        if comments:
+            last = comments[-1]
+            disc = IConversation(last)
+            return disc.total_comments
+
+    return replynum
+
+
+@indexer(IObservation)
+def conclusion1_reply_number(context):
+    replynum = 0
+    conclusions = context.values(['Conclusion'])
+    if conclusions:
+        conclusion = conclusions[0]
+        disc = IConversation(conclusion)
+        return disc.total_comments
+
+    return replynum
+
+
+@indexer(IObservation)
+def conclusion2_reply_number(context):
+    replynum = 0
+    conclusions = context.values(['ConclusionsPhase2'])
+    if conclusions:
+        conclusion = conclusions[0]
+        disc = IConversation(conclusion)
+        return disc.total_comments
+
+    return replynum
