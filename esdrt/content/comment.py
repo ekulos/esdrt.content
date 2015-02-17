ca1bee0a6a828a08f962f6009944d0ae4caecdf0
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-02-17 07:58:59 +0100

    whitespace

--- esdrt/content/comment.py
+++ esdrt/content/comment.py
@@ -165,7 +165,6 @@ def add_question(context, event):
         if api.content.get_state(observation) == 'phase2-draft':
             api.content.transition(obj=observation, transition='phase2-open')
 
-
         if api.content.get_state(observation).startswith('phase1-'):
             context.manage_addProperty('creator_role', 'Sector Expert', 'string')
         else:
